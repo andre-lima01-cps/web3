7445f800716ea73669b18a0e55ed6dff48bd8374
André Lima <andre.lima01@cps.sp.gov.br>
2026-03-22 16:23:06 -0300

    Pequenos ajustes

--- consulta.php
+++ consulta.php
@@ -9,9 +9,16 @@
     <h1>Lista de Usuários</h1>
     <table border='1'>
     <?php
+    // Inicia o buffer de saída para capturar o conteúdo gerado pela consulta_bd.php
     ob_start();
+
+    // Inclui o arquivo de consulta que gera a saída JSON dos usuários
     include 'backend/consulta_bd.php';
+
+    // Captura o conteúdo gerado pela consulta_bd.php
     $data = ob_get_clean();
+    
+    // Decodifica o JSON para um array associativo
     $usuarios = json_decode($data, true);
 
     echo "<tr><th>ID</th><th>Nome</th><th>Email</th><th>Role</th><th>Created At</th><th>Updated At</th></tr>";
